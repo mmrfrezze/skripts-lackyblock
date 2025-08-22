7eebda92bc1f28b2af8743dafedd75b83eec3740
xikibamboni <mmrfrezze@gmail.com>
2025-08-22 09:57:38 +0200

    xiki.lua

--- xiki.lua
+++ xiki.lua
@@ -1,4 +1,3 @@
--- Инициализация
 local cam = workspace.Camera
 local plrs = game.Players
 local plr = plrs.LocalPlayer
@@ -17,7 +16,6 @@ local bestSwords = {
     "FireSword","CrescendoTheSoulStealer","FangOfTsuchigumo"
 }
 
-
 local function kill(target)
     local pHRP = target.Character.HumanoidRootPart
     local lastCF = plr.Character.HumanoidRootPart.CFrame
@@ -29,7 +27,6 @@ local function kill(target)
     plr.Character.HumanoidRootPart.CFrame = lastCF
 end
 
-
 local function equipBest()
     for _, tool in pairs(plr.Backpack:GetChildren()) do
         for _, sword in pairs(bestSwords) do
@@ -40,12 +37,11 @@ local function equipBest()
     end
 end
 
-
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 local Window = Rayfield:CreateWindow({
-    Name = "Xiki | Lucky Block Battleground",
-    LoadingTitle = "Xiki UI Loading...",
-    LoadingSubtitle = "by xikibamboni",
+    Name = "🟣 Xiki | Lucky Block BG",
+    LoadingTitle = "Xiki UI",
+    LoadingSubtitle = "Modern & Clean Design",
     ConfigurationSaving = {
         Enabled = true,
         FolderName = "Xiki UI",
@@ -55,34 +51,23 @@ local Window = Rayfield:CreateWindow({
     KeySystem = false
 })
 
-
 local CharTab = Window:CreateTab("Character", 4483362458)
 local LuckyTab = Window:CreateTab("Lucky Blocks", 4483362458)
-
 local TimeTab = Window:CreateTab("Clock Time", 4483362458)
 local AdvTab = Window:CreateTab("Advanced", 4483362458)
-
 
 CharTab:CreateSection("Movement")
 CharTab:CreateSlider({Name="Walk Speed", Range={0,100}, Increment=1, CurrentValue=16, Tooltip="Изменяет скорость передвижения", Callback=function(v) plr.Character.Humanoid.WalkSpeed=v end})
 CharTab:CreateButton({Name="Reset Walk Speed", Callback=function() plr.Character.Humanoid.WalkSpeed=16 end})
 CharTab:CreateSlider({Name="Jump Power", Range={0,1000}, Increment=1, CurrentValue=50, Tooltip="Изменяет силу прыжка", Callback=function(v) plr.Character.Humanoid.JumpPower=v end})
 CharTab:CreateButton({Name="Reset Jump Power", Callback=function() plr.Character.Humanoid.JumpPower=50 end})
-CharTab:CreateToggle({Name="Infinite Jumps", CurrentValue=false, Tooltip="Позволяет прыгать бесконечно", Callback=function(v) 
-    infJump=v 
-    if v then 
-        uis.JumpRequest:Connect(function() 
-            if infJump then plr.Character.Humanoid:ChangeState("Jumping") end 
-        end) 
-    end 
-end})
+CharTab:CreateToggle({Name="Infinite Jumps", CurrentValue=false, Tooltip="Позволяет прыгать бесконечно", Callback=function(v) infJump=v if v then uis.JumpRequest:Connect(function() if infJump then plr.Character.Humanoid:ChangeState("Jumping") end end) end end})
 
 CharTab:CreateSection("Environment")
 CharTab:CreateSlider({Name="Gravity", Range={0,1000}, Increment=1, CurrentValue=196, Tooltip="Меняет силу гравитации", Callback=function(v) workspace.Gravity=v end})
 CharTab:CreateButton({Name="Reset Gravity", Callback=function() workspace.Gravity=196 end})
 CharTab:CreateSlider({Name="Field of View", Range={1,120}, Increment=1, CurrentValue=70, Tooltip="Меняет FOV камеры", Callback=function(v) cam.FieldOfView=v end})
 CharTab:CreateButton({Name="Reset FOV", Callback=function() cam.FieldOfView=70 end})
-
 
 local function spawnBlock(event)
     replic[event]:FireServer(1)
@@ -108,12 +93,10 @@ LuckyTab:CreateSection("Galaxy Lucky Block 🌌")
 LuckyTab:CreateButton({Name="Spawn", Callback=function() spawnBlock("SpawnGalaxyBlock") end})
 LuckyTab:CreateToggle({Name="Auto Spawn", CurrentValue=false, Callback=function(v) as_glb=v; while as_glb do task.wait(0.1) spawnBlock("SpawnGalaxyBlock") end end})
 
-
 TimeTab:CreateButton({Name="Night", Callback=function() game.Lighting.ClockTime=0 end})
 TimeTab:CreateButton({Name="Day", Callback=function() game.Lighting.ClockTime=12 end})
 TimeTab:CreateButton({Name="Evening", Callback=function() game.Lighting.ClockTime=18 end})
 TimeTab:CreateButton({Name="Morning", Callback=function() game.Lighting.ClockTime=6 end})
-
 
 AdvTab:CreateSection("Weapons")
 AdvTab:CreateButton({Name="Equip All Tools", Callback=function() for _, tool in pairs(plr.Backpack:GetChildren()) do tool.Parent=plr.Character task.wait() end end})
