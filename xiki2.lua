617de5141681d6e32fae9bae0bb9329b435dcac9
xikibamboni <mmrfrezze@gmail.com>
2025-08-24 09:48:11 +0200

    xiki2.lua

--- xiki2.lua
+++ xiki2.lua
@@ -1,4 +1,3 @@
-
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 local VirtualInputManager = game:GetService("VirtualInputManager")
 local Players = game:GetService("Players")
